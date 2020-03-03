5ddee21414c6ca4cef6e47663a75a49f294bd35e
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-03-03 14:10:41 -0600

    "Added .cs211 folder and pasted in template model"

--- src/model.hxx
+++ src/model.hxx
@@ -1,8 +1,57 @@
 #pragma once
 
+#include <iostream>
+#include "player.hxx"
 #include <ge211.hxx>
 
-//
-// TODO: Sketch the structure of your model and declare its operations.
-//
+// Logical state of the game
+class Model
+{
+public:
 
+    // constructs a model with two players and a width and height for the dimensions of the stage
+    explicit Model(Player player1, Player player2_, int width, int height);
+
+    // sets the velocity of the player
+    void set_p1_velocity(ge211::Position);
+
+    // sets the velocity of the player
+    void set_p2_velocity(ge211::Position);
+
+    // makes the  1st player do their attack based on if it is charged up or not
+    void p1_attack();
+
+    // makes the 2nd player do their attack based on if it is charged up or not
+    void p2_attack();
+
+    // checks if either player gets hit by the other and does the necessary animations if they are hit
+    void check_for_hit();
+
+    // makes the 1st player go into a state of blocking
+    void p1_block();
+
+    // makes the 2nd player go into a state of blocking
+    void p2_block();
+
+    // checks if either player has 0 health and makes the game over with the helper if it returns true
+    bool health_check();
+
+    // checks to make sure players do not go through each other
+    void check_collision();
+
+    // gets the health of the 1st player
+    int check_p1_health();
+
+    // gets the health of the 2nd player
+    int check_p2_health();
+
+private:
+
+    friend struct Test_access;
+    Player winner_ = Player::neither;
+    Player player1_;
+    Player player2_;
+
+    // declares winner and resets the game to original positions
+    void game_over();
+};
