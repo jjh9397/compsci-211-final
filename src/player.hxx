a00e54e9dbfb12ba76e787c2d843503999e6a04c
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-03-03 15:15:18 -0600

    Added fields to player header

--- src/player.hxx
+++ src/player.hxx
@@ -6,6 +6,21 @@
 class Player
 {
     public:
+    explicit Player(ge211::Position, ge211::Position)
+
+    void hitbox_next();
+    void hurtbox_next();
+    void hits_side();
+    void collides(Player);
 
     private:
+    ge211::Rectangle hitbox;
+    ge211::Rectangle hurtbox;
+    bool blocking;
+    bool active;
+    ge211::Position hitbox_velocity;
+    ge211::Position hurtbox_velocity;
+    int health;
+
+    friend struct Test_access;
 };
