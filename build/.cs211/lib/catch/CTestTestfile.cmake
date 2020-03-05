f4aae71faaed77022d00f21151c2bde69ebdf720
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-03-04 22:59:15 -0600

    Removed build

--- build/.cs211/lib/catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jason/Documents/CS211/compsci-211-final/.cs211/lib/catch
-# Build directory: C:/Users/Jason/Documents/CS211/compsci-211-final/build/.cs211/lib/catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/.cs211/lib/ge211/3rdparty/utf8-cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jason/Documents/CS211/compsci-211-final/.cs211/lib/ge211/3rdparty/utf8-cpp
-# Build directory: C:/Users/Jason/Documents/CS211/compsci-211-final/build/.cs211/lib/ge211/3rdparty/utf8-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
