74bac2a41a910b52a8174eb4c605792f938d02eb
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-03-04 19:37:32 -0600

    Fixed view constructor

--- src/view.cxx
+++ src/view.cxx
@@ -4,6 +4,9 @@
 
 View::View(Model const &model)
     : model_(model)
-    , hitbox()
-    , hurtbox()
+    , hitbox({50,100}, {0,0,0})
+    , hurtbox({10,50}, {255,255,255})
+    , p1(ge211::Position(400,20), ge211::Position(400,20), ge211::Dimensions({1,0}))
+    , p2(ge211::Position(400,20), ge211::Position(400,20), ge211::Dimensions({-1,0}))
 {}
+    
