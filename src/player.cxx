88fb136d35f7a4ad4171c9652aadc3835cd1a230
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-03-04 19:07:52 -0600

    Added getters and setters

--- src/player.cxx
+++ src/player.cxx
@@ -2,14 +2,14 @@
 #include "player.hxx"
 #include <ge211.hxx>
 
-Player::Player(ge211::Position hit_pos, ge211::Position hurt_pos, ge211::Dimensions direction)
+Player::Player(ge211::Position hit_pos, ge211::Position hurt_pos, ge211::Dimensions facing)
         : hitbox({50,100})
         , hitbox_velocity({0,0})
         , hurtbox_velocity({0,0})
         , blocking(false)
         , active(false)
         , health(100)     
-        , facing(direction)
+        , direction(facing)
 {
     hitbox.top_left() = hit_pos;
     hitbox.top_right() = hurt_pos;
@@ -56,4 +56,68 @@ bool Player::hits_bottom()
     {
         return false;
     }
+}
+
+bool Player::take_damage(int damage)
+{
+    health -= damage;
+    if (health < 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+int Player::get_health()
+{
+    return health;
+}
+    
+//void Player::set_hitbox_position(ge211::Position)
+ge211::Position Player::get_hitbox_position()
+{
+    return hitbox.top_left();
+}
+
+//void Player::set_hurtbox_position(ge211::Position)
+ge211::Position Player::get_hurtbox_position()
+{
+    return hurtbox.top_left();
+}
+    
+void Player::set_hitbox_velocity(ge211::Dimensions vel)
+{
+    hitbox_velocity = vel;
+}
+ge211::Dimensions Player::get_hitbox_velocity()
+{
+    return hitbox_velocity;
+}
+
+void Player::set_hurtbox_velocity(ge211::Dimensions vel)
+{
+    hurtbox_velocity = vel;
+}
+void Player::set_hurtbox_angular_velocity(int vel)
+{
+    hurtbox_angular_velocity = vel;
+}
+ge211::Dimensions Player::get_hurtbox_velocity()
+{
+    return hurtbox_velocity;
+}
+int Player::get_hurtbox_angular_velocity()
+{
+    return hurtbox_angular_velocity;
+}
+
+void Player::set_direction(ge211::Dimensions dir)
+{
+    direction = dir;
+}
+ge211::Dimensions Player::get_direction()
+{
+    return direction;
 }
