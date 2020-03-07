88d95ea60c08321318bcf238c52665368f0bfb54
jjiang316 <jjiang316@gmail.com>
2020-03-06 20:02:26 -0600

    remove cmake folder

--- cmake-build-debug/.cs211/lib/ge211/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jason/Documents/CS211/compsci-211-final/.cs211/lib/ge211
-# Build directory: C:/Users/Jason/Documents/CS211/compsci-211-final/cmake-build-debug/.cs211/lib/ge211
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("3rdparty/utf8-cpp")
-subdirs("examples/installed")
